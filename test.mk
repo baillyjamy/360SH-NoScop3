e658bd37ffa87174f42fc3d6dd284d6d4894db99
Valentin Pichard <valentin.pichard@epitech.eu>
2016-04-16 13:48:35 +0200

    {ADD} test Makefile

---
+++ test.mk
@@ -0,0 +1,24 @@
+##
+## test.mk for  in /home/antoine
+##
+## Made by antoine
+## Login   <antoine@epitech.net>
+##
+## Started on  Thu Mar  3 10:39:22 2016 antoine
+## Last update Sat Apr 16 13:47:52 2016 Valentin Pichard
+##
+
+TEST_SOURCES_NAMES	= test_generic.c \
+			test_int_to_string.c \
+			test_parse_int.c \
+			test_string_concat.c \
+			test_string_find.c \
+			test_string_find_char.c \
+			test_string_type.c \
+			test_string_type_convert.c \
+			test_suites.c 
+
+TEST_SOURCES		= $(foreach file,$(TEST_SOURCES_NAMES), \
+			test/$(file))
+
+TEST_OBJECTS		= $(TEST_SOURCES:.c=.o)
