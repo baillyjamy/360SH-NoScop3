f5cecd693326f8233b357ed9cbc9f00e118d00a7
Valentin Pichard <valentin.pichard@epitech.eu>
2016-04-22 17:51:50 +0200

    add src folder

---
+++ src/shell.c
@@ -0,0 +1,14 @@
+/*
+** shell.c for  in /Users/pichar_v/Documents/tek1/42SH/360SH-NoScop3/src/
+**
+** Made by Valentin Pichard
+** Login   <pichar_v@epitech.eu>
+**
+** Started on  Fri Apr 22 17:49:55 2016 Valentin Pichard
+** Last update Fri Apr 22 17:50:26 2016 Valentin Pichard
+*/
+
+int		main(int argc, char **argv, char **envp)
+{
+  return (0);
+}
