030cacc36ad0612a18af6b8a96a47c35be521954
Valentin Pichard <valentin.pichard@epitech.eu>
2016-05-19 23:29:11 +0200

    Add colors in prompt

--- display.c
+++ display.c
@@ -5,11 +5,12 @@
 ** Login   <pichar_v@epitech.eu>
 **
 ** Started on  Thu May 19 23:13:05 2016 Valentin Pichard
-** Last update Thu May 19 23:23:41 2016 Valentin Pichard
+** Last update Thu May 19 23:28:32 2016 Valentin Pichard
 */
 
 #include "sh.h"
 #include <stdio.h>
+#include "../colorize/colorize.h"
 
 void		display_environment(const char **envp)
 {
@@ -22,5 +23,5 @@ void		display_environment(const char **envp)
 
 void		display_prompt(const char *user)
 {
-  printf("%s@360SH-NoScop3 ~ $", user);
+  printf("%s%s%s@360SH-NoScop3 ~ $ ", FG_RED, user, RESET);
 }
