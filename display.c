e31fec5ef045967fe45ce6ad800cfe7f62922aa3
Valentin Pichard <valentin.pichard@epitech.eu>
2016-05-20 15:59:14 +0200

    Patch colorize path + switch printf to fprintf

--- display.c
+++ display.c
@@ -5,12 +5,12 @@
 ** Login   <pichar_v@epitech.eu>
 **
 ** Started on  Thu May 19 23:13:05 2016 Valentin Pichard
-** Last update Thu May 19 23:28:32 2016 Valentin Pichard
+** Last update Fri May 20 15:54:53 2016 Valentin Pichard
 */
 
 #include "sh.h"
 #include <stdio.h>
-#include "../colorize/colorize.h"
+#include "colorize/colorize.h"
 
 void		display_environment(const char **envp)
 {
@@ -18,10 +18,10 @@ void		display_environment(const char **envp)
 
   i = 0;
   while (envp[i])
-    printf("%s\n", envp[i]);
+    fprintf(stdout, "%s\n", envp[i]);
 }
 
 void		display_prompt(const char *user)
 {
-  printf("%s%s%s@360SH-NoScop3 ~ $ ", FG_RED, user, RESET);
+  fprintf(stdout, "%s%s%s@360SH-NoScop3 ~ $ ", FG_RED, user, RESET);
 }
