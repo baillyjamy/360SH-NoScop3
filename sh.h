9f27c32f355aee8ea62b6a72d018702673dd341a
Valentin Pichard <valentin.pichard@epitech.eu>
2016-05-24 16:12:29 +0200

    Use egc_exit

--- sh.h
+++ sh.h
@@ -5,7 +5,7 @@
 ** Login   <antoine@epitech.net>
 **
 ** Started on  Sun Apr 17 16:42:03 2016 antoine
-** Last update Fri May 20 16:08:39 2016 Valentin Pichard
+** Last update Tue May 24 16:03:45 2016 Valentin Pichard
 */
 
 #ifndef SH_H
@@ -14,6 +14,6 @@
 # include "egc.h"
 # include "toolbox/tools.h"
 
-# define GOODBYE(x) exit(x)
+# define GOODBYE(x) egc_exit(x)
 
 #endif /* SH_H */
