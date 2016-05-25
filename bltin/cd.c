a2f6699065099a78a0e3e2da4e885a6ce0a5eb0b
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2016-05-25 15:09:05 +0200

    Add bltin folder and cd.c

---
+++ bltin/cd.c
@@ -0,0 +1,26 @@
+/*
+** cd.c for  in /home/jacqui_p/rendu/PSU/360SH-NoScop3/bltin/
+**
+** Made by Pierre-Emmanuel Jacquier
+** Login   <jacqui_p@epitech.eu>
+**
+** Started on  Tue May 24 15:48:33 2016 Pierre-Emmanuel Jacquier
+** Last update Tue May 24 15:48:33 2016 Pierre-Emmanuel Jacquier
+*/
+
+#include "hs.h"
+#include "glist_hs.h"
+
+int	cdcmd(t_glist_hs *argv)
+{
+  if (glist_hs_length(argv) == 2)
+    {
+      if (hs_equals(glist_hs_get(args, 0), hs("-")))
+	return (cd_less());
+      else
+	return (cd_path());
+    }
+  else if(glist_hs_length(argv) == 1)
+    return (cd_home());
+  return (0);
+}
