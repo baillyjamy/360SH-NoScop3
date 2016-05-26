d20cbfca638abe992cd4fc5a031b0b8e547b12ad
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2016-05-26 17:14:25 +0200

    Add cd_home() and cd_path()

--- bltin/cd.c
+++ bltin/cd.c
@@ -11,7 +11,7 @@
 #include <unistd.h>
 #include "../egc.h"
 
-int	cd_less(void)
+static int	cd_less(void)
 {
   t_hs	old_pwd;
   t_hs	current_pwd;
@@ -24,14 +24,44 @@ int	cd_less(void)
   return (1);
 }
 
+static int	cd_path(t_hs path)
+{
+  t_hs		current_pwd;
+
+  if (chdir_error(path))
+    return (1);
+  current_pwd = get_env_line(hs("PWD"));
+  set_env_line(hs_concat(hs("OLDPWD"), current_pwd));
+  chdir(hs_to_str(path));
+  set_env_line(hs_concat(hs("PWD"), path));
+  return (1);
+}
+
+static int	cd_home(void)
+{
+  t_hs		home_path;
+  t_hs		current_pwd;
+
+  current_pwd = get_env_line(hs("PWD"));
+  home_path = get_env_line(hs("HOME"));
+  if (home_error(home_path))
+    return (1);
+  chdir(hs_to_str(home_path));
+  set_env_line(hs_concat(hs("PWD"), home_path));
+  set_env_line(hs_concat(hs("OLDPWD"), current_pwd));
+  return (1);
+}
+
 int	cdcmd(t_glist_hs *argv)
 {
+  if (glist_hs_length(argv) > 2)
+    return (egc_fprintf(STDERR_FILENO, "cd: Too many arguments.\n"));
   if (glist_hs_length(argv) == 2)
     {
-      if (hs_equals(glist_hs_get(argv, 0), hs("-")))
+      if (hs_equals(glist_hs_get(argv, 1), hs("-")))
 	return (cd_less());
       else
-	return (cd_path());
+	return (cd_path(glist_hs_get(argv, 1)));
     }
   else if(glist_hs_length(argv) == 1)
     return (cd_home());
