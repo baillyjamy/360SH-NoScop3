af6b6e49cea4390601ff904477c186f8a25f2366
Antoine Motet <motet_a@epitech.eu>
2016-05-27 22:53:01 +0200

    Improve set_variable() function in setenv.c

--- bltin/setenv.c
+++ bltin/setenv.c
@@ -12,40 +12,47 @@
 #include <stdio.h>
 #include "../sh.h"
 
-static int	errors_name(t_hs name)
+static int      is_valid_variable_name(t_hs name)
 {
-  unsigned	i;
-  char	c;
+  int           i;
+  char          c;
 
-  i = 0;
-  if (!char_is_alpha(hs_get(name, 0)))
+  i = -1;
+  while (++i < (int)hs_length(name))
+    {
+      c = hs_get(name, i);
+      if (!char_is_alpha_numeric(c) || !char_is_digit(c))
+        return (0);
+    }
+  return (1);
+}
+
+static int	check_variable_name(t_hs name)
+{
+  if (!hs_length(name) || !char_is_alpha(hs_get(name, 0)))
     {
       fprintf(stderr, "setenv: Variable name must begin with a letter.\n");
       return (1);
     }
-  while (i < hs_length(name))
+  if (!is_valid_variable_name(name))
     {
-      c = hs_get(name, i++);
-      if (!char_is_alpha(c) || !char_is_digit(c) || c != '_')
-	{
-	  fprintf(stderr,
-                  "setenv: Variable name must"
-		  "contain alphanumeric characters.\n");
-	  return (1);
-	}
+      fprintf(stderr,
+              "setenv: Variable name must contain alphanumeric "
+              "characters.\n");
+      return (1);
     }
   return (0);
 }
 
-static int	set_variable(t_hs name, t_hs value, short value_is_null)
+static int	set_variable(t_hs name, t_hs value, int value_is_null)
 {
-  if (!errors_name(name))
-    {
-      if (value_is_null)
-	set_env_line(name, value, 1);
-      set_env_line(name, value, 0);
-    }
-  return (1);
+  if (!check_variable_name(name))
+    return (1);
+
+  if (value_is_null)
+    set_env_line(name, value, 1);
+  set_env_line(name, value, 0);
+  return (0);
 }
 
 int	setenv_cmd(t_glist_hs *argv)
