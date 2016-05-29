f993863b5b378c78f1ed2c7c29e67db7c4e477eb
Pierre-Emmanuel Jacquier <pierre-emmanuel.jacquier@epitech.eu>
2016-05-29 21:26:35 +0200

    Add envcmd() to env_cmd.c to bltin

---
+++ bltin/env_cmd.c
@@ -0,0 +1,16 @@
+/*
+** env_cmd.c for  in /home/jacqui_p/rendu/PSU/360SH-NoScop3/bltin/
+**
+** Made by Pierre-Emmanuel Jacquier
+** Login   <jacqui_p@epitech.eu>
+**
+** Started on  Sun May 29 21:10:04 2016 Pierre-Emmanuel Jacquier
+** Last update Sun May 29 21:10:04 2016 Pierre-Emmanuel Jacquier
+*/
+
+#include "../sh.h"
+
+void	envcmd(void)
+{
+  display_environment();
+}
