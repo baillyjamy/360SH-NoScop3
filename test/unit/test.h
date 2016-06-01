602da215624007db5f126db88ac6ac0fbfcb8455
Antoine Motet <motet_a@epitech.eu>
2016-06-01 16:16:04 +0200

    Fix norm errors in test/

--- test/unit/test.h
+++ test/unit/test.h
@@ -8,8 +8,8 @@
 ** Last update Wed Jun 01 01:19:53 2016 Antoine Baudrand
 */
 
-#ifndef TEST_H
-# define TEST_H
+#ifndef TEST_H_
+# define TEST_H_
 
 # include "../../sh.h"
 
@@ -42,4 +42,4 @@ void    test_suite_colorize(void);
 void    test_suite_exec(void);
 void    test_suite_lexer(void);
 
-#endif /* TEST_H */
+#endif /* !TEST_H_ */
