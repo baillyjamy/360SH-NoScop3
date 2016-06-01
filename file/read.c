df2eac618835893ca47e9ae4b03acee5443bb71f
Valentin Pichard <valentin.pichard@epitech.eu>
2016-06-01 16:31:12 +0200

    Fix read_file norm

--- file/read.c
+++ file/read.c
@@ -5,7 +5,7 @@
 ** Login   <pichar_v@epitech.eu>
 **
 ** Started on  Mon May 30 00:15:40 2016 Valentin Pichard
-** Last update Tue May 31 15:48:27 2016 Valentin Pichard
+** Last update Wed Jun  1 15:33:53 2016 Valentin Pichard
 */
 
 #include "file.h"
@@ -30,9 +30,8 @@ int		read_file(const t_hs filename, t_glist_hs *lines)
   t_glist_hs	new_lines;
 
   i = 0;
-  if ((file_length = get_file_length(filename)) == -1)
-    return (-1);
-  if ((data = egc_malloc(file_length + 1)) == NULL)
+  if ((file_length = get_file_length(filename)) == -1 &&
+     (data = egc_malloc(file_length + 1)) == NULL)
     return (-1);
   if ((fd = open_file(filename, O_RDONLY)) == -1)
     return (-1);
