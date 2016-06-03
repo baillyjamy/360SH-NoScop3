9e29dc501618f50ebc3192722d0bbb274f5ec5c8
Bailly <jamy.bailly@epitech.eu>
2016-06-03 17:04:27 +0200

    [Readline] Add ctrl + d

--- readline/readline_event.c
+++ readline/readline_event.c
@@ -10,6 +10,7 @@
 
 #include "private.h"
 #include "string.h"
+#include "../sh.h"
 
 static void	readline_escape_envent(t_readline *readline, char *c)
 {
@@ -31,6 +32,11 @@ static void	readline_ascii_event(t_capacity *capacity, char c)
 {
   if (c == 0x0C)
     egc_printf("%s", capacity->capacity_clear_screen);
+  else if (c == 0x04)
+    {
+      egc_printf("exit\n");
+      exitcmd(0);
+    }
   else
     return ;
 }
