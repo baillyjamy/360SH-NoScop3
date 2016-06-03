b593bbed1466d3e8614a449966c2fd1dc151c16c
Antoine Motet <motet_a@epitech.eu>
2016-06-03 17:43:53 +0200

    Improve parser.h

--- parser/parser.h
+++ parser/parser.h
@@ -12,7 +12,6 @@
 # define PARSER_H_
 
 # include "../lexer/lexer.h"
-# include "../exec.h"
 # include "../sh.h"
 
 typedef enum    e_node_type
@@ -23,9 +22,13 @@ typedef enum    e_node_type
   NODE_AND,
   NODE_OR,
   NODE_BACKGROUND,
+  NODE_LIST,
 }               t_node_type;
 
 char const      *node_type_to_str(t_node_type type);
+
+struct                  s_node;
+typedef struct s_node   t_node;
 
 /*
 ** `type` is the type of the node. The followings types define
@@ -35,6 +38,9 @@ char const      *node_type_to_str(t_node_type type);
 **      left: The node at the left of the pipeline
 **      right: The node at the right of the pipeline
 **      pipe_background: True if `|&` was used, false if `|` was used.
+**
+** NODE_COMMAND:
+**      args: The command name and its arguments
 **
 ** NODE_PAREN:
 **      left: The child node, between the parens
@@ -51,28 +57,42 @@ char const      *node_type_to_str(t_node_type type);
 **      left: The node at the left of `||`
 **      right: The node at the right of of `||`
 **
+** NODE_LIST:
+**      children: The children nodes (separated by semicolons)
+**
 ** <all node types>:
 **      type: The type of the node.
-**      input, error_output, output: File descriptors.
+**      input, error_output, output: File descriptors. If one of these
+**      file descriptors is not equal to 0, 1 or 2, it is open and should
+**      be closed.
 **
 ** The other properties are left undefined.
 */
-typedef struct  s_node
+struct  s_node
 {
   t_node_type   type;
-  t_exec        command;
+  int           input;
+  int           error_output;
+  int           output;
   int           pipe_background;
-  t_hs          file_in;
-  t_hs          file_out;
-  struct s_node *left;
-  struct s_node *right;
-}               t_node;
+  t_node        *left;
+  t_node        *right;
+  t_glist_hs    args;
+  t_glist_voidp children;
+};
+
+/*
+** Creates a new node.
+**
+** Initializes only the property shared by all the node types.
+*/
+t_node  *node_new(t_node_type type);
 
 /*
 ** Returns a string describing the node, for debugging purposes.
 */
 t_hs    node_to_hs(const t_node *node);
-t_node  *add_node(t_node_type type, t_node *left);
-void    parse(t_token_list *tokens);
+
+t_node  *parse(t_token_list *tokens);
 
 #endif /* PARSER_H_ */
