c92427655c4f234dddefdc3d7720dc1d34abf9d9
Valentin Pichard <valentin.pichard@epitech.eu>
2016-06-03 17:36:04 +0200

    Add rcfile exist/creator

--- file/file.h
+++ file/file.h
@@ -5,7 +5,7 @@
 ** Login   <pichar_v@epitech.eu>
 **
 ** Started on  Sun May 29 22:12:46 2016 Valentin Pichard
-** Last update Thu Jun  2 16:19:55 2016 Valentin Pichard
+** Last update Fri Jun  3 17:35:20 2016 Valentin Pichard
 */
 
 #ifndef FILE_H_
@@ -76,5 +76,6 @@ void		close_file(int fd);
 int		edit_file(const t_hs filename);
 ssize_t		get_file_length(const t_hs filename);
 int		file_exist(const t_hs filename);
+int		rcfile_exist(void);
 
 #endif /* !FILE_H_ */
--- rcfile.c
+++ rcfile.c
@@ -5,7 +5,20 @@
 ** Login   <pichar_v@epitech.eu>
 **
 ** Started on  Thu Jun  2 16:50:13 2016 Valentin Pichard
-** Last update Thu Jun  2 16:50:25 2016 Valentin Pichard
+** Last update Fri Jun  3 17:34:07 2016 Valentin Pichard
 */
 
 #include "sh.h"
+
+int		rcfile_exist(void)
+{
+  if (file_exist(hs("~/.360sh-noscop3rc")) == 0)
+    return (0);
+  else
+    {
+      if (create_file(hs("~/.360sh-noscop3rc")) == 0)
+	return (1);
+      else
+	return (-1);
+    }
+}
