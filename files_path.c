fcd6169fb31016bb1b0bc54ba6d7721bbbeaaec3
Valentin Pichard <valentin.pichard@epitech.eu>
2016-06-07 16:30:27 +0200

    Add motd path

--- files_path.c
+++ files_path.c
@@ -5,7 +5,7 @@
 ** Login   <pichar_v@epitech.eu>
 **
 ** Started on  Sun Jun  5 21:06:04 2016 Valentin Pichard
-** Last update Sun Jun  5 21:07:56 2016 Valentin Pichard
+** Last update Tue Jun  7 16:29:35 2016 Valentin Pichard
 */
 
 #include "sh.h"
@@ -18,4 +18,9 @@ t_hs		path_history(void)
 t_hs		path_rcfile(void)
 {
   return (hs("./.360sh-noscop3rc"));
+}
+
+t_hs		path_motd(void)
+{
+  return (hs("./.360sh-noscop3_motd"));
 }
